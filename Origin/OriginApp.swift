08fabffa8a4d5e547baad2e92f1aa2cf7d41c424
Milosh <vavro.milosh@gmail.com>
2026-09-14 22:14:00 +0200

    Initial Commit

---
+++ Origin/OriginApp.swift
@@ -0,0 +1,17 @@
+//
+//  OriginApp.swift
+//  Origin
+//
+//  Created by Milosh on 14/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct OriginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
